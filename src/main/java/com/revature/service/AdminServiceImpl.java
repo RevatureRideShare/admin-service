8977f211c3fc540145600c965e3ceced76674fa8
Jane <thejaneshin@gmail.com>
2019-11-27 09:18:27 -0500

    finished service methods

--- src/main/java/com/revature/service/AdminServiceImpl.java
+++ src/main/java/com/revature/service/AdminServiceImpl.java
@@ -1,12 +1,17 @@
 package com.revature.service;
 
 import com.revature.bean.Admin;
+import com.revature.exception.DeleteNonexistentException;
+import com.revature.exception.UpdateNonexistentException;
 import com.revature.repo.AdminRepo;
 
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.ConstraintViolationException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,28 +25,41 @@ public class AdminServiceImpl implements AdminService {
   }
 
   @Override
-  public Admin createAdmin(Admin admin) {
-    Admin returnAdmin = adminRepo.save(admin);
-    return returnAdmin;
+  public Admin createAdmin(Admin admin)
+      throws IllegalArgumentException, ConstraintViolationException {
+    if (getAdmin(admin.getAdminID()).isPresent()) {
+      throw new DuplicateKeyException("Object already exists in database");
+    } else {
+      return adminRepo.save(admin);
+    }
   }
 
   @Override
-  public void deleteAdmin(Admin admin) {
-
+  public void deleteAdmin(Admin admin) throws IllegalArgumentException {
+    if (!getAdmin(admin.getAdminID()).isPresent()) {
+      throw new DeleteNonexistentException(admin + " does not exist and cannot be deleted");
+    } else {
+      adminRepo.delete(admin);
+    }
   }
 
   @Override
-  public Admin updateAdmin(Admin admin) {
-    return null;
+  public Admin updateAdmin(Admin admin)
+      throws IllegalArgumentException, ConstraintViolationException {
+    if (!getAdmin(admin.getAdminID()).isPresent()) {
+      throw new UpdateNonexistentException(admin + " does not exist and cannot be updated");
+    } else {
+      return adminRepo.save(admin);
+    }
   }
 
   @Override
   public Optional<Admin> getAdmin(int adminID) {
-    return null;
+    return adminRepo.findById(adminID);
   }
 
   @Override
   public List<Admin> getAllAdmins() {
-    return null;
+    return adminRepo.findAll();
   }
 }
