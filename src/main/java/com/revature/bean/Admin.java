8d06821895b897a19717fb18a4522442800f29c5
Astralvix <tsukasa992@yahoo.com>
2019-11-28 08:55:50 -0500

    finished integration testing for adminService #10

--- src/main/java/com/revature/bean/Admin.java
+++ src/main/java/com/revature/bean/Admin.java
@@ -129,32 +129,43 @@ public class Admin {
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj)
+    if (this == obj) {
       return true;
-    if (obj == null)
+    }
+    if (obj == null) {
       return false;
-    if (getClass() != obj.getClass())
+    }
+    if (getClass() != obj.getClass()) {
       return false;
+    }
     Admin other = (Admin) obj;
-    if (accountStatus != other.accountStatus)
+    if (accountStatus != other.accountStatus) {
       return false;
-    if (adminID != other.adminID)
+    }
+    if (adminID != other.adminID) {
       return false;
+    }
     if (email == null) {
-      if (other.email != null)
+      if (other.email != null) {
         return false;
-    } else if (!email.equals(other.email))
+      }
+    } else if (!email.equals(other.email)) {
       return false;
+    }
     if (firstName == null) {
-      if (other.firstName != null)
+      if (other.firstName != null) {
         return false;
-    } else if (!firstName.equals(other.firstName))
+      }
+    } else if (!firstName.equals(other.firstName)) {
       return false;
+    }
     if (lastName == null) {
-      if (other.lastName != null)
+      if (other.lastName != null) {
         return false;
-    } else if (!lastName.equals(other.lastName))
+      }
+    } else if (!lastName.equals(other.lastName)) {
       return false;
+    }
     return true;
   }
 
--- src/test/java/com/revature/test/AdminServiceImplIntegrationTest.java
+++ src/test/java/com/revature/test/AdminServiceImplIntegrationTest.java
@@ -1,9 +1,16 @@
 package com.revature.test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.revature.bean.Admin;
+import com.revature.exception.DeleteNonexistentException;
+import com.revature.exception.UpdateNonexistentException;
 import com.revature.service.AdminServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -12,6 +19,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.TransactionSystemException;
 
 @SpringBootTest
 class AdminServiceImplIntegrationTest {
@@ -23,6 +34,14 @@ class AdminServiceImplIntegrationTest {
   private Admin updatedAdmin;
   private Admin changedAdmin;
   private Admin nullAdmin;
+  private Admin badFormatEmptyEmailAdmin;
+  private Admin badFormatMaxSizeEmailAdmin;
+  private Admin badFormatNotEmailAdmin;
+  private Admin badFormatEmptyFirstNameAdmin;
+  private Admin badFormatMaxSizeFirstNameAdmin;
+  private Admin badFormatEmptyLastNameAdmin;
+  private Admin badFormatMaxSizeLastNameAdmin;
+  private Admin newAdminWithExistingEmail;
 
   @Autowired
   public void setAdminServiceImpl(AdminServiceImpl adminServiceImpl) {
@@ -41,8 +60,24 @@ class AdminServiceImplIntegrationTest {
 
   @BeforeEach
   void setUp() throws Exception {
-
-
+    nullAdmin = null;
+    existingAdmin = new Admin(1, "admin@revature.com", "John", "Deer", true);
+    updatedAdmin = new Admin(2, "admin2@revature.com", "Not", "Updated", true);
+    nonExistingAdmin = new Admin(999, "admin@nonexistant,com", "Dont", "Existance", true);
+    changedAdmin = new Admin(2, "admin2@revature.com", "I Am", "Updated", true);
+    newAdmin = new Admin(4, "entertheadmin@revature.com", "Animatrix", "Woke", true);
+    badFormatEmptyEmailAdmin = new Admin(20, "", "Nobody", "Jones", true);
+    badFormatMaxSizeEmailAdmin =
+        new Admin(20, "anAbsurdlyLongEmailThatShouldntBeThisLongButJustInCase@revature.com",
+            "Nobody", "Jones", true);
+    badFormatNotEmailAdmin = new Admin(20, "thisisntanemail", "Nobody", "Jones", true);
+    badFormatEmptyFirstNameAdmin = new Admin(20, "amazingAdmin@admin.com", "", "Jones", true);
+    badFormatMaxSizeFirstNameAdmin = new Admin(20, "amazingAdmin@admin.com",
+        "This is my absurdly long first name ive had it ever since I was a child", "Jones", true);
+    badFormatEmptyLastNameAdmin = new Admin(20, "amazingAdmin@admin.com", "Nobody", "", true);
+    badFormatMaxSizeLastNameAdmin = new Admin(20, "amazingAdmin@admin.com", "Nobody",
+        "My last name is so long I have no idea what to do with it, but I guess Ill live", true);
+    newAdminWithExistingEmail = new Admin(10, "admin@revature.com", "John", "Deer", true);
   }
 
   @AfterEach
@@ -51,8 +86,128 @@ class AdminServiceImplIntegrationTest {
   }
 
   @Test
-  void test() {
-    fail("Not yet implemented");
+  @Sql("admin-script.sql")
+  void testGetExistingAdminById() {
+    assertEquals(adminServiceImpl.getAdmin(existingAdmin.getAdminID()), Optional.of(existingAdmin));
   }
 
+  @Test
+  @Sql("admin-script.sql")
+  void testGetAllTrainingLocation() {
+    List<Admin> existingAdminList = new ArrayList<>();
+    existingAdminList.add(existingAdmin);
+    existingAdminList.add(updatedAdmin);
+    assertEquals(adminServiceImpl.getAllAdmins(), existingAdminList);
+
+    System.out.println(adminServiceImpl.getAllAdmins());
+  }
+
+  @Test
+  @Sql("admin-script.sql")
+  void testCreateNewAdmin() {
+    Admin extraNewAdmin = adminServiceImpl.createAdmin(newAdmin);
+    assertEquals(Optional.of(extraNewAdmin), adminServiceImpl.getAdmin(extraNewAdmin.getAdminID()));
+  }
+
+  @Test
+  @Sql("admin-script.sql")
+  void testCreateExistingAdmin() {
+    assertThrows(DuplicateKeyException.class, () -> {
+      adminServiceImpl.createAdmin(existingAdmin);
+    });
+  }
+
+  @Test
+  void testCreateNullAdmin() {
+    assertThrows(NullPointerException.class, () -> {
+      adminServiceImpl.createAdmin(nullAdmin);
+    });
+  }
+
+  @Test
+  @Sql("admin-script.sql")
+  void testDeleteExistingAdmin() {
+    adminServiceImpl.deleteAdmin(existingAdmin);
+    assertEquals(adminServiceImpl.getAdmin(existingAdmin.getAdminID()), Optional.empty());
+  }
+
+  @Test
+  void testDeleteNonExistingAdmin() {
+    assertThrows(DeleteNonexistentException.class, () -> {
+      adminServiceImpl.deleteAdmin(nonExistingAdmin);
+    });
+  }
+
+  @Test
+  @Sql("admin-script.sql")
+  void testUpdateExistingAdmin() {
+    System.out.println(
+        "Current state of updatedAdmin" + adminServiceImpl.getAdmin(updatedAdmin.getAdminID()));
+    adminServiceImpl.updateAdmin(changedAdmin);
+    assertEquals(adminServiceImpl.getAdmin(changedAdmin.getAdminID()), Optional.of(changedAdmin));
+  }
+
+  @Test
+  void testUpdateNonExistingAdmin() {
+    assertThrows(UpdateNonexistentException.class, () -> {
+      adminServiceImpl.updateAdmin(nonExistingAdmin);
+    });
+  }
+
+  @Test
+  void testCreateBadFormatEmptyEmailAdmin() {
+    assertThrows(TransactionSystemException.class, () -> {
+      adminServiceImpl.createAdmin(badFormatEmptyEmailAdmin);
+    });
+  }
+
+  @Test
+  void testCreateBadFormatMaxSizeEmailAdmin() {
+    assertThrows(TransactionSystemException.class, () -> {
+      adminServiceImpl.createAdmin(badFormatMaxSizeEmailAdmin);
+    });
+  }
+
+  @Test
+  void testCreateBadFormatNotAnEmailAdmin() {
+    assertThrows(TransactionSystemException.class, () -> {
+      adminServiceImpl.createAdmin(badFormatNotEmailAdmin);
+    });
+  }
+
+  @Test
+  void testCreateBadFormatEmptyFirstNameAdmin() {
+    assertThrows(TransactionSystemException.class, () -> {
+      adminServiceImpl.createAdmin(badFormatEmptyFirstNameAdmin);
+    });
+  }
+
+  @Test
+  void testCreateBadFormatMaxSizeFirstNameAdmin() {
+    assertThrows(TransactionSystemException.class, () -> {
+      adminServiceImpl.createAdmin(badFormatMaxSizeFirstNameAdmin);
+    });
+  }
+
+  @Test
+  void testCreateBadFormatEmptyLastNameAdmin() {
+    assertThrows(TransactionSystemException.class, () -> {
+      adminServiceImpl.createAdmin(badFormatEmptyLastNameAdmin);
+    });
+  }
+
+  @Test
+  void testCreateBadFormatMaxSizeLastNameAdmin() {
+    assertThrows(TransactionSystemException.class, () -> {
+      adminServiceImpl.createAdmin(badFormatMaxSizeLastNameAdmin);
+    });
+  }
+
+  @Test
+  @Sql("admin-script.sql")
+  void testCreateNewAdminWithSameEmail() {
+    assertThrows(DataIntegrityViolationException.class, () -> {
+      adminServiceImpl.createAdmin(newAdminWithExistingEmail);
+    });
+  }
 }
